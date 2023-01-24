dd5e1c87883da8d90912e62f48b4b7d229d50e33
Antonio Cheong <acheong@student.dalat.org>
2023-01-24 23:44:19 +0800

    Add more properties to agent

--- types/agent.go
+++ types/agent.go
@@ -5,4 +5,6 @@ type Agent struct {
 	Identity  string `json:"identity"`
 	PublicKey string `json:"public_key"`
 	PublicIP  string `json:"public_ip"`
+	Country   string `json:"country"`
+	ISP       string `json:"isp"`
 }
