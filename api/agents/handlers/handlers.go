fa5b9b78c415a06652ed04a879ed8269daef57a1
Antonio Cheong <acheong@student.dalat.org>
2023-01-24 23:40:54 +0800

    Refactoring

--- api/agents/handlers/handlers.go
+++ api/agents/handlers/handlers.go
@@ -5,6 +5,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 
 	"github.com/RunawayVPN/Runaway-Hub/tools/auth"
 	sec "github.com/RunawayVPN/Runaway-Hub/tools/security"
@@ -14,14 +15,9 @@ import (
 
 const SECRET_KEY = "secret"
 
-type RegistrationPayload struct {
-	PublicIP  string `json:"public_ip"`
-	SecretKey string `json:"secret_key"`
-	PublicKey string `json:"public_key"`
-	Name      string `json:"name"`
-}
 type UnverifiedRequest struct {
 	PublicKey string `json:"public_key"`
+	SecretKey string `json:"secret_key"`
 	JwtToken  string `json:"jwt"`
 }
 type RegisterAgentResponse struct {
@@ -33,70 +29,59 @@ type RegisterAgentResponse struct {
 
 func RegisterAgent(c *gin.Context) {
 	var raw_request UnverifiedRequest
-	var response RegisterAgentResponse
 	err := c.BindJSON(&raw_request)
 	if err != nil {
-		response.Success = false
-		response.Error = err.Error()
-		c.JSON(400, response)
+		c.JSON(400, RegisterAgentResponse{
+			Success: false,
+			Error:   err.Error(),
+		})
 		return
 	}
+	// Check secret key
+	if raw_request.SecretKey != SECRET_KEY {
+		if check_error(c, errors.New("invalid secret key"), 500) {
+			return
+		}
+	}
 	// Verify JWT
-	request_jwt_payload, err := sec.VerifyToken(raw_request.JwtToken, raw_request.PublicKey)
-	if err != nil {
-		response.Success = false
-		response.Error = err.Error()
-		c.JSON(401, response)
+	request_payload, err := sec.VerifyToken(raw_request.JwtToken, raw_request.PublicKey)
+	if check_error(c, err, 401) {
 		return
 	}
 	// Unmarshal payload
-	var request_payload RegistrationPayload
-	err = json.Unmarshal([]byte(request_jwt_payload), &request_payload)
-	if err != nil {
-		response.Success = false
-		response.Error = err.Error()
-		c.JSON(500, response)
+	var agent types.Agent
+	err = json.Unmarshal([]byte(request_payload), &agent)
+	if check_error(c, err, 500) {
 		return
-	}
-	if request_payload.SecretKey != SECRET_KEY {
-		response.Success = false
-		response.Error = "Invalid secret key"
-		c.JSON(401, response)
-		return
-	}
-	// Register agent
-	agent := types.Agent{
-		PublicIP:  request_payload.PublicIP,
-		PublicKey: request_payload.PublicKey,
-		Name:      request_payload.Name,
-		Identity:  "agent",
 	}
 	err = auth.RegisterAgent(agent)
-	if err != nil {
-		response.Success = false
-		response.Error = err.Error()
-		c.JSON(500, response)
+	if check_error(c, err, 500) {
 		return
 	}
-	// Construct response
-	response.Success = true
-	response.PublicKey = sec.EncodeBS(sec.Public_key)
 	// Construct JWT payload with agent
 	response_jwt_payload, err := json.Marshal(agent)
-	if err != nil {
-		response.Success = false
-		response.Error = err.Error()
-		c.JSON(500, response)
+	if check_error(c, err, 500) {
 		return
 	}
 	// Create JWT response_token
 	response_token, err := sec.CreateToken(string(response_jwt_payload))
-	if err != nil {
-		response.Success = false
-		response.Error = err.Error()
-		c.JSON(500, response)
+	if check_error(c, err, 500) {
 		return
 	}
-	response.JwtToken = response_token
-	c.JSON(200, response)
+	c.JSON(200, RegisterAgentResponse{
+		Success:   true,
+		PublicKey: sec.EncodeBS(sec.Public_key),
+		JwtToken:  response_token,
+	})
+}
+
+func check_error(c *gin.Context, err error, code int) bool {
+	if err != nil {
+		c.JSON(code, RegisterAgentResponse{
+			Success: false,
+			Error:   err.Error(),
+		})
+		return true
+	}
+	return false
 }
